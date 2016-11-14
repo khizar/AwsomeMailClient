430beeb1adb3678449d0aab344cd8a3aeee49052
khizar <khizar.aziz@humly.de>
2016-11-14 16:14:23 +0100

    bug fix

--- src/components/MailMessage/MailMessage.jsx
+++ src/components/MailMessage/MailMessage.jsx
@@ -4,9 +4,15 @@ import classNames from 'classnames';
 const MailMessage = (props) => (
     <div className="message-container">
         <div className="email-text">
-            <span className="sender">{props.email ? 'From: ' + props.email.sender : ''}</span><br/>
-            <span className="subject">{props.email ? 'Subject: ' + props.email.subject : ''}</span><br/>
-            <span className={classNames({'center-text': !props.email })}>{props.email ? props.email.message: 'Select an email'}</span>
+            <span className="sender">
+                {props.email && props.email.sender ? 'From: ' + props.email.sender : ''}
+            </span><br/>
+            <span className="subject">
+                {props.email && props.email.subject ? 'Subject: ' + props.email.subject : ''}
+            </span><br/>
+            <span className={classNames({'center-text': !(props.email && props.email.message)})}>
+                {props.email && props.email.message ? props.email.message: 'Select an email'}
+            </span>
         </div>
     </div>
 );
